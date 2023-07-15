eacbc2c7497e37578ac96fb62f370f1b4de58ed8
menthy-wu <wumengxuan0728@gmail.com>
2023-07-14 22:54:53 -0700

    mentor

--- bot/main.py
+++ bot/main.py
@@ -9,16 +9,22 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext.commands import Bot, has_permissions
+from discord import app_commands
 from .roles import Roles
 from .scheduler import Scheduler
+from .mentor import Mentor
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+VERIFICATION_CHANNEL_ID = os.getenv("DISCORD_VERIFICATION_CHANNEL_ID")
+MENTOR_CHANNEL_ID = os.getenv("DISCORD_MENTOR_CHANNEL_ID")
+ROLE_CHANNEL_ID = os.getenv("DISCORD_ROLE_CHANNEL_ID")
 
 bot: discord.Client = Bot(command_prefix="!", intents=discord.Intents.all())
 
 roles = Roles(bot)
 scheduler = Scheduler(bot)
+mentor = Mentor(bot)
 
 
 @bot.tree.command(name="roles")
@@ -29,17 +35,37 @@ async def send_role_assignment(ctx: discord.Interaction):
         await roles.send_role_assignment(ctx)
 
 
+@bot.tree.command(name="find_a_mentor")
+@app_commands.describe(
+    location=
+    "let me know where is your physical locaiton so a mentor can come find you"
+)
+@app_commands.describe(tech="let me know what tech stack are you using")
+@app_commands.describe(additional="do you have additional notes want to add")
+async def on_request_send(ctx: discord.Interaction, location: str, tech: str,
+                          additional: str):
+    """Mentor request"""
+    await mentor.on_request_send(ctx, location, tech, additional)
+
+
 @bot.event
 async def on_reaction_add(reaction: discord.Reaction, user: discord.Member):
     """Add Reaction Based Role"""
-    await roles.on_reaction_add(reaction, user)
+    if reaction.message.author == user:
+        return
+    if str(reaction.message.channel.id) == VERIFICATION_CHANNEL_ID:
+        await roles.on_reaction_add(reaction, user)
+    elif str(reaction.message.channel.id) == MENTOR_CHANNEL_ID or str(
+            reaction.message.channel.id) == ROLE_CHANNEL_ID:
+        await mentor.on_reaction_add(reaction, user)
 
 
 @bot.event
 async def on_ready():
     """Run when the bot initially loads"""
     try:
-        # await bot.tree.sync()
+        # sync =  await bot.tree.sync()
+        # print(sync)
         print("bot is ready")
         while True:
             await scheduler.send_reminders()
---
+++ bot/mentor.py
@@ -0,0 +1,46 @@
+"""allow hacker to request for mentor and mentors to accept request"""
+import os
+from dotenv import load_dotenv
+import discord
+
+load_dotenv()
+
+
+class Mentor:
+    """handle mentor request and assinemnt"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.mentor_channel = os.getenv("DISCORD_MENTOR_CHANNEL_ID")
+        self.mentee_channel = os.getenv("DISCORD_MENTEE_CHANNEL_ID")
+
+    async def on_request_send(self, ctx: discord.Interaction, location, tech,
+                              other):
+        """command when hacker request for a mentor"""
+        mentor_text = f"{ctx.user.mention} needs help with {tech} please react 🤚 if you can \nlocation: {location}\n{ ''if other=='' else f'additional notes:{other}'}"
+        mentee_text = f"{ctx.user.mention}, you requested help with {tech} \nlocation: {location}\n{ ''if other=='' else f'additional notes:{other}'}"
+        mentee_channel: discord.TextChannel = await self.bot.fetch_channel(
+            self.mentee_channel)
+        mentor_channel: discord.TextChannel = await self.bot.fetch_channel(
+            self.mentor_channel)
+        message: discord.Message = await mentor_channel.send(mentor_text)
+        await message.add_reaction("🤚")
+        await mentee_channel.send(mentee_text)
+
+    async def on_reaction_add(self, reaction: discord.Reaction,
+                              user: discord.Member):
+        """send a message to mentee when a mentor react on their request"""
+        mentee_channel: discord.TextChannel = await self.bot.fetch_channel(
+            self.mentee_channel)
+        mentor_channel: discord.TextChannel = await self.bot.fetch_channel(
+            self.mentor_channel)
+        if reaction.emoji == "🤚":
+            mentee_text = f"{user.mention} has accepted your request, they are on their way to help you! If they do not come in 10 mins you can try contact the on discord or send another request"
+            mentor_text = f"{user.mention}, you have accepted {reaction.message.mentions[0].mention} 's request, react ✅ when you resolve the issues"
+            await mentee_channel.send(mentee_text)
+            mentor_message: discord.Message = await mentor_channel.send(
+                mentor_text)
+            await mentor_message.add_reaction("✅")
+        elif reaction.emoji == "✅":
+            mentee_text = f"{user.mention} has reslved your issue"
+            await mentee_channel.send(mentee_text)
